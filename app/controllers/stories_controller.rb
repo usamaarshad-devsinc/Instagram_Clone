9a621ec569d51f48ce7d2e0a6e61a32206a672b2
Usama Arshad <usama.arshad@devsinc.com>
2021-09-14 17:27:48 +0500

    Stories CRUD Basic CRUD implemented * Still needs some UI adjustment for stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,19 +1,50 @@
 class StoriesController < ApplicationController
+
+  def index
+    @stories = [].concat current_account.stories
+    requests = current_account.requests_sent.where(status: 'accepted')
+    requests.each do |req|
+      @stories.concat(req.recipient.stories)
+    end
+  end
+
   def new
     @story = Story.new
   end
 
   def create
-    @story = current_account.stories.create(description: story_params)
-    render @story
+    @story = current_account.stories.create(story_params)
+    redirect_to @story
   end
 
   def show
+    @story = Story.find(params[:id])
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    if @story.destroy
+      redirect_to controller: :public, action: :homepage, notice: 'Story was successfuly deleted.'
+    end
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    puts story_params
+    if @story.update!(story_params)
+      redirect_to @story, notice: 'Story was successfuly updated.'
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def story_params
-    params.require(:story).permit(:description)
+    params.require(:story).permit(:description,:image)
   end
 end
