010a99b295991a7b19f5175cd90c170fef5d3559
Usama Arshad <usama.arshad@devsinc.com>
2021-09-15 12:01:52 +0500

    Account model Validation * For presence of extra attributes added in Devise (e.g. username, fullname)

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,7 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :presence_check
   has_one_attached :profile_pic
   has_many :stories, dependent: :destroy
   has_many :posts, dependent: :destroy
@@ -11,4 +12,10 @@ class Account < ApplicationRecord
   has_many :requests_recieved, foreign_key: 'recipient_id', class_name: 'Request', dependent: :destroy
   has_many :requests_sent, foreign_key: 'sender_id', class_name: 'Request', dependent: :destroy
 
+  def presence_check
+    # For extra fields added in Devise.
+    if [self.full_name, self.username].reject(&:blank?).size < 2
+      errors[:base] << ("Please give both - fullname and username.")
+    end
+  end
 end
