03ab8d356f09d3d2ac4873ff050d02cc2f5cb9fe
Usama Arshad <usama.arshad@devsinc.com>
2021-09-15 17:28:49 +0500

    Validation in Post's model * Can't create an empty post (no description and no image)

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,20 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many_attached :images, dependent: :destroy
-  validate :restrict_number_of_images
+  validate :presence_check
 
   def restrict_number_of_images
     if images.size > 10
       errors[:base] << ("Post can't have more than 10 images.")
+    end
+  end
+
+  def presence_check
+    # For extra fields added in Devise.
+    if [self.images, self.description].reject(&:blank?).size == 0
+      errors[:base] << ("You can't create an empty post [no description and no image].")
+    else
+      restrict_number_of_images
     end
   end
 end
