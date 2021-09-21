07674c70ca1b212247d34c41edd3dea562403042
Usama Arshad <usama.arshad@devsinc.com>
2021-09-21 18:54:47 +0500

    Unfollow [Bug-Fixed]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def already_liked?(post_id)
     Like.exists?(account_id: current_account.id, post_id: post_id)
   end
+
+  def find_request(recipient)
+    current_account.requests_sent.find_by(recipient_id: recipient.id)
+  end
 end
