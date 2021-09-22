66bfb6dbcc6360672f0de60bfab8931343b1549a
Usama Arshad <usama.arshad@devsinc.com>
2021-09-22 16:29:23 +0500

    Development.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,22 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
+  # Mailer Configurations for Gmail
+  config.action_mailer.default_url_options = { host: 'https://instagram-clone-devsinc.herokuapp.com/', port: -5000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'localhost',
+    user_name: Rails.application.credentials.dig(:gmail_smtp, :email),
+    password: Rails.application.credentials.dig(:gmail_smtp, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
