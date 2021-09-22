668124f9003ca19b7da2119518490a0dd35367b9
Usama Arshad <usama.arshad@devsinc.com>
2021-09-22 12:55:55 +0500

    Cleaned unused Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,14 @@
 Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "public#homepage"
   get 'search', to: 'public#search'
-  # resources :posts
-  # resources :likes
-  # resources :comments
-  resources :requests
-  resources :stories, only: %i[destroy create new show index]
-  post 'privacy', to: 'requests#toggle_privacy'
+  resources :requests, only: %i[create update destroy]
+  resources :stories, except: %i[edit update]
   get 'profile', to: 'public#profile'
 
   root 'posts#index'
   resources :posts, shallow: true do
-    resources :comments
-    resources :likes
+    resources :comments, except: %i[index new show]
+    resources :likes, only: %i[create destroy]
   end
 end
